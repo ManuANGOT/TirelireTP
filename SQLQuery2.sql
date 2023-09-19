90a755dfabf8764c56cf15a0ab947f17a9d86a71
Manu ANGOT <manu.angothotmail.fr>
2023-09-19 16:05:28 +0200

    création models

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,3 +1,37 @@
+CREATE TABLE Customer (
+	id_customer INT PRIMARY KEY IDENTITY(1,1),
+	customer_name NVARCHAR (50) NOT NULL,
+	customer_firstname NVARCHAR(50) NOT NULL,
+	customer_birthdate DATETIME NOT NULL,
+	customer_password NVARCHAR(100) NOT NULL,
+);
+
+CREATE TABLE Product (
+	id_product INT PRIMARY KEY IDENTITY(1,1),
+	product_name NVARCHAR(50) NOT NULL,
+	product_description NVARCHAR(1000) NOT NULL,
+	product_height NVARCHAR(20) NOT NULL,
+	product_width NVARCHAR(20) NOT NULL,
+	product_weight NVARCHAR(20) NOT NULL,
+	product_lenght NVARCHAR(20) NOT NULL,
+	product_color NVARCHAR(50) NOT NULL,
+	product_capacity NVARCHAR(50) NOT NULL,
+	product_maker NVARCHAR(50) NOT NULL,
+	product_img IMAGE NOT NULL,
+	product_price FLOAT NOT NULL,
+);
+
+CREATE TABLE Shopping_cart(
+cart_id INT PRIMARY KEY IDENTITY(1,1),
+cart_date DATETIME NOT NULL,
+cart_product_added INT NOT NULL,
+cart_product_cancelled INT NOT NULL,
+cart_details INT NOT NULL,
+cart_shipping INT NOT NULL,
+cart_price INT NULL,
+
+);
+
 CREATE TABLE TirelireRoles (
     RoleId INT PRIMARY KEY IDENTITY(1,1),
     RoleName NVARCHAR(50) NOT NULL
---
+++ TirelireProject/TirelireProject/Views/CustomerViews/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TirelireProject.Views.CustomerViews
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
